dafdc8062fa4f4c37d4016d0a9b9289a4879e83d
Jessica Lane Ramos <41808261+JessicaRamoslc@users.noreply.github.com>
2020-07-01 06:17:44 -0300

    Add files via upload

---
+++ smtleads/js/script.js
@@ -0,0 +1,32 @@
+function captarLeads() {
+   // var leads = [];
+   // var leadForm = {
+   //      "name": document.getElementById('nameForm').value, 
+   //      "email": document.getElementById('emailForm').value
+   // }
+
+   
+   
+
+   // for(let i = 0; i < leads.length; i++){
+   //  leads.push(leadForm); 
+   // }
+
+   // console.log(leads.push(leadForm));   
+
+
+   
+
+       
+//     console.log(leads);
+//    alert("Oi")
+   
+ 
+//    leadForm.idade = document.getElementById();
+//    leadForm.profissao = document.getElementById();
+
+   
+
+   
+   
+
